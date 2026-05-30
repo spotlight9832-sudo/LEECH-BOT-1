d8aa2367ba0daf9936543185f5383a7927006f31
spotlight9832-sudo <spotlight9832@gmail.com>
2026-05-30 21:33:16 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -3,9 +3,9 @@ class KPSMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = '🤖 Repo'
-    ST_BN1_URL = 'https://github.com/Tamilupdates/KPSML-X'
+    ST_BN1_URL = 'https://t.me/Eren_Yeager_76'
     ST_BN2_NAME = 'Updates 🔥'
-    ST_BN2_URL = 'https://telegram.me/KPSBots'
+    ST_BN2_URL = 'https://t.me/Eren_Yeager_76'
     ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
 Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
